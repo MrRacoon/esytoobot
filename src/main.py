c3a3ee34473fe690f59cb8ba7e203cfabad2a405
Erik Sutherland <erik.misu@gmail.com>
2018-11-23 23:33:13 -0800

    fix typo in name

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
-from unit_manager import GroupManager
+from unit_manager import ProtossBot
 
 run_game(
     maps.get("(2)DreamcatcherLE"), [
-        Bot(Race.Protoss, GroupManager()),
+        Bot(Race.Protoss, ProtossBot()),
         # Human(Race.Terran),
         Computer(Race.Zerg, Difficulty.Easy)
      ],
